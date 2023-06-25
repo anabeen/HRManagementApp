5ca00b2362fa6d01190fb25cb7c1d1d4fa423f22
nabeen <axnabeen@gmail.com>
2023-06-25 14:01:30 -0500

    moved the employee class and creatd inheretence of employee class

---
+++ HRManagementApp/HR/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagementApp.HR
+{
+    public class Manager: Employee
+    {
+        public Manager(string first, string last, double rate): base(first, last, rate) 
+        {
+            
+        }
+
+    }
+}
